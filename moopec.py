c2096800bdafb339678c712c2fa7085a7227c9bf
Rui Mendes <azuki@di.uminho.pt>
2021-10-07 09:02:05 +0100

    Update moopec.py
    Added requirement for Python version >= 3.8

--- moopec.py
+++ moopec.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import sys
+assert sys.version_info.major == 3 and sys.version_info.minor >= 8, "This script needs at least version 3.8"
 import moopec_parser
 if __name__ == '__main__':
     import sys
